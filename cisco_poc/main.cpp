92644bb10dd0e00f10338efd1eeb211a23927647
Lejo Varughese <email@lejovarughese.com>
2015-07-20 16:22:42 -0500

    Lejo - Initial Commit : Basic hello world with fruit. Assuming fruit is already instealled.

--- cisco_poc/main.cpp
+++ cisco_poc/main.cpp
@@ -3,13 +3,62 @@
 //  cisco_poc
 //
 //  Created by Lejo on 7/20/15.
-//  Copyright (c) 2015 cisco. All rights reserved.
+//  Copyright (c) 2015 Cisco. All rights reserved.
 //
 
 #include <iostream>
+#include "fruit/fruit.h"
+
+using fruit::Component;
+using fruit::Injector;
+
+class Writer {
+public:
+    virtual void write(std::string s) = 0;
+};
+
+
+class StdoutWriter : public Writer {
+public:
+    INJECT(StdoutWriter()) = default;
+    
+    virtual void write(std::string s) override {
+        std::cout << s;
+    }
+};
+        
+class Greeter {
+public:
+    virtual void greet() = 0;
+};
+
+class GreeterImpl : public Greeter {
+private:
+    Writer* writer;
+    
+public:
+    INJECT(GreeterImpl(Writer* writer))
+    : writer(writer) {
+    }
+    
+    virtual void greet() override {
+        writer->write("Greetings!\n");
+    }
+};
+        
+Component<Greeter> getGreeterComponent() {
+    return fruit::createComponent()
+    .bind<Writer, StdoutWriter>()
+    .bind<Greeter, GreeterImpl>();
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Injector<Greeter> injector(getGreeterComponent());
+    Greeter* greeter = injector.get<Greeter*>();
+    
+    greeter->greet();
+    
     return 0;
 }
+        
+        
